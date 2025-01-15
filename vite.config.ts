22a70a51ac8f0401dc3c3fe7a6ab86ffad9828be
zhaoteng <1813831021@qq.com>
2025-01-15 18:53:30 +0800

    修复

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,15 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import path from 'path'
-import { fileURLToPath } from 'url'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/yili-vehicle-management/',
   plugins: [vue()],
   resolve: {
     alias: {
-      '@': path.resolve(fileURLToPath(new URL('.', import.meta.url)), 'src')
-    }
+      '@': path.resolve(__dirname, 'src'),
+    },
   },
   server: {
     port: 3000,
