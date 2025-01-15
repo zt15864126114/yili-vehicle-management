02603757bb047c321b8e2ea35ffb8ecd2c7f1731
zt15864126114 <40525198+zt15864126114@users.noreply.github.com>
2025-01-15 10:54:11 +0000

    Deploying to gh-pages from @ zt15864126114/yili-vehicle-management@22a70a51ac8f0401dc3c3fe7a6ab86ffad9828be 🚀

--- assets/driver-B9Ek07UP.js
+++
@@ -1,1 +0,0 @@
-import{M as e}from"./mock-BnFOwhRk.js";const r=e.mock({"drivers|10":[{"id|+1":1,name:"@cname",licenseNumber:/[A-Z0-9]{12}/,licenseExpireDate:'@date("yyyy-MM-dd")',phoneNumber:/1[3-9]\d{9}/,"status|1":["available","onDuty","leave"],entryDate:'@date("yyyy-MM-dd")',"drivingYears|3-20":1}]});export{r as d};
---
+++ assets/vehicle-DpWRqzrQ.js
@@ -0,0 +1,1 @@
+import{M as e}from"./mock-CdG3V9JI.js";const t=e.mock({"vehicles|20":[{"id|+1":1,plateNumber:/鲁Q[A-Z0-9]{5}/,"model|1":["五十铃","解放J6","重汽豪沃","东风天龙"],purchaseDate:'@date("yyyy-MM-dd")',insuranceExpireDate:'@date("yyyy-MM-dd")',"status|1":["normal","maintenance","scrapped"]}]});export{t as v};
