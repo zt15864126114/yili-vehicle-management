d6b20a2da88f6cdb74664d5040b6a6410a118889
zt15864126114 <40525198+zt15864126114@users.noreply.github.com>
2025-01-15 10:43:56 +0000

    Deploying to gh-pages from @ zt15864126114/yili-vehicle-management@0d774706a354f977cac7fef33f426619e362cb41 🚀

---
+++ assets/fuelRecord-siKxubu8.js
@@ -0,0 +1,1 @@
+const e={records:[{id:"1",vehicleId:"1",driverId:"1",fuelDate:"2024-03-15 08:30:00",fuelType:"92#汽油",fuelVolume:45.5,unitPrice:7.23,cost:328.97,station:"中石化兖州站",currentMileage:15680}]};export{e as f};
